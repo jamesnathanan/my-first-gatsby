dd65592f3aded9d38d3ec36b69b4c6655234e5fc
jamesnathanan <57458881+jamesnathanan@users.noreply.github.com>
2021-07-28 20:13:52 +0700

    Gatsby

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link, navigate } from 'gatsby';
+import styled from 'styled-components';
+import Logo from './Logo';
 
 // function goToSliceMasters() {
 //   //   1. wait for 2 sec
@@ -31,9 +33,52 @@ import { Link, navigate } from 'gatsby';
 //   );
 // }
 
+const NavStyles = styled.nav`
+  margin-bottom: 3rem;
+  .logo {
+    transform: translateY(-25%);
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    text-align: center;
+    margin-top: -6rem;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr auto 1fr 1fr;
+    grid-gap: 2rem;
+    align-items: center;
+  }
+  li {
+    --rotate: -2deg;
+    transform: rotate(var(--rotate));
+    order: 1;
+    &:nth-child(1) {
+      --rotate: 1deg;
+    }
+    &:nth-child(2) {
+      --rotate: -2.5deg;
+    }
+    &:nth-child(4) {
+      --rotate: 2deg;
+    }
+    &:hover {
+      --rotate: 3deg;
+    }
+  }
+  a {
+    font-size: 3rem;
+    text-decoration: none;
+    &:hover {
+      color: var(--red);
+    }
+  }
+`;
+
 export default function Nav() {
   return (
-    <nav>
+    <NavStyles>
       <ul>
         <li>
           <Link to="/">Hot Now</Link>
@@ -42,7 +87,9 @@ export default function Nav() {
           <Link to="/pizzas/">Pizza Menu!</Link>
         </li>
         <li>
-          <Link to="/">LOGO</Link>
+          <Link to="/">
+            <Logo />
+          </Link>
         </li>
         <li>
           <Link to="/slicemasters">SliceMasters</Link>
@@ -51,6 +98,6 @@ export default function Nav() {
           <Link to="/order">Order Ahead !</Link>
         </li>
       </ul>
-    </nav>
+    </NavStyles>
   );
 }
